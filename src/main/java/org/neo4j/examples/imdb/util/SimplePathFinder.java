c36335fd94958123e30f12b454f67a4008cf48e6
Peter Neubauer <peter@neubauer.se>
2010-09-06 11:30:57 +0200

    upgrading finder to new tarversal framework

--- src/main/java/org/neo4j/examples/imdb/util/SimplePathFinder.java
+++ src/main/java/org/neo4j/examples/imdb/util/SimplePathFinder.java
@@ -9,114 +9,101 @@ import java.util.Map;
 
 import org.neo4j.graphdb.Direction;
 import org.neo4j.graphdb.Node;
+import org.neo4j.graphdb.Path;
 import org.neo4j.graphdb.RelationshipType;
-import org.neo4j.graphdb.ReturnableEvaluator;
-import org.neo4j.graphdb.StopEvaluator;
-import org.neo4j.graphdb.TraversalPosition;
-import org.neo4j.graphdb.Traverser;
-import org.neo4j.graphdb.Traverser.Order;
+import org.neo4j.graphdb.traversal.TraversalDescription;
+import org.neo4j.graphdb.traversal.Traverser;
+import org.neo4j.helpers.Predicate;
+import org.neo4j.kernel.Traversal;
+import org.neo4j.kernel.impl.traversal.TraversalDescriptionImpl;
 
-public class SimplePathFinder implements PathFinder
-{
-    private static final int MAXIMUM_DEPTH = 5;
+public class SimplePathFinder implements PathFinder {
+	private static final int MAXIMUM_DEPTH = 5;
 
-    public List<Node> shortestPath( final Node startNode, final Node endNode,
-        final RelationshipType relType )
-    {
-        return findPath( startNode, endNode, relType );
-    }
+	public List<Node> shortestPath(final Node startNode, final Node endNode,
+			final RelationshipType relType) {
+		return findPath(startNode, endNode, relType);
+	}
 
-    private List<Node> findPath( final Node startNode, final Node endNode,
-        final RelationshipType relType )
-    {
-        final Map<Node,Node> traversedNodes1 = new HashMap<Node,Node>();
-        final Map<Node,Node> traversedNodes2 = new HashMap<Node,Node>();
-        final StopEvaluator stopEval = new PathStopEval();
-        final PathReturnEval returnEval1 = new PathReturnEval( traversedNodes1,
-            traversedNodes2 );
-        final PathReturnEval returnEval2 = new PathReturnEval( traversedNodes2,
-            traversedNodes1 );
-        final Traverser trav1 = startNode.traverse( Order.BREADTH_FIRST,
-            stopEval, returnEval1, relType, Direction.BOTH );
-        final Traverser trav2 = endNode.traverse( Order.BREADTH_FIRST,
-            stopEval, returnEval2, relType, Direction.BOTH );
-        Iterator<Node> itr1 = trav1.iterator();
-        Iterator<Node> itr2 = trav2.iterator();
-        while ( itr1.hasNext() || itr2.hasNext() )
-        {
-            if ( itr1.hasNext() )
-            {
-                itr1.next();
-            }
-            if ( returnEval1.getMatch() != null )
-            {
-                List<Node> path = returnEval1.getMatch();
-                Collections.reverse( path );
-                return path;
-            }
-            if ( itr2.hasNext() )
-            {
-                itr2.next();
-            }
-            if ( returnEval2.getMatch() != null )
-            {
-                return returnEval2.getMatch();
-            }
-        }
-        return Collections.emptyList();
-    }
+	private List<Node> findPath(final Node startNode, final Node endNode,
+			final RelationshipType relType) {
+		final Map<Node, Node> traversedNodes1 = new HashMap<Node, Node>();
+		final Map<Node, Node> traversedNodes2 = new HashMap<Node, Node>();
+		final PathReturnEval returnEval1 = new PathReturnEval(traversedNodes1,
+				traversedNodes2);
+		final PathReturnEval returnEval2 = new PathReturnEval(traversedNodes2,
+				traversedNodes1);
+		TraversalDescription td1 = new TraversalDescriptionImpl()
+				.order(Traversal.postorderBreadthFirst())
+				.prune(Traversal.pruneAfterDepth(MAXIMUM_DEPTH))
+				.filter(returnEval1)
+				.expand(Traversal.expanderForTypes(relType, Direction.BOTH));
+		final Traverser trav1 = td1
+				.traverse(startNode);
+		TraversalDescription td2 = new TraversalDescriptionImpl()
+				.order(Traversal.postorderBreadthFirst())
+				.prune(Traversal.pruneAfterDepth(MAXIMUM_DEPTH))
+				.filter(returnEval2)
+				.expand(Traversal.expanderForTypes(relType, Direction.BOTH));
+		final Traverser trav2 = td2
+				.traverse(endNode);
+		Iterator<Node> itr1 = trav1.nodes().iterator();
+		Iterator<Node> itr2 = trav2.nodes().iterator();
+		while (itr1.hasNext() || itr2.hasNext()) {
+			if (itr1.hasNext()) {
+				itr1.next();
+			}
+			if (returnEval1.getMatch() != null) {
+				List<Node> path = returnEval1.getMatch();
+				Collections.reverse(path);
+				return path;
+			}
+			if (itr2.hasNext()) {
+				itr2.next();
+			}
+			if (returnEval2.getMatch() != null) {
+				return returnEval2.getMatch();
+			}
+		}
+		return Collections.emptyList();
+	}
 
-    private static class PathStopEval implements StopEvaluator
-    {
-        public boolean isStopNode( TraversalPosition currentPos )
-        {
-            return currentPos.depth() >= MAXIMUM_DEPTH;
-        }
-    }
+	private static class PathReturnEval implements Predicate<Path> {
+		private final Map<Node, Node> myNodes;
+		private final Map<Node, Node> otherNodes;
+		private LinkedList<Node> match = null;
 
-    private static class PathReturnEval implements ReturnableEvaluator
-    {
-        private final Map<Node,Node> myNodes;
-        private final Map<Node,Node> otherNodes;
-        private LinkedList<Node> match = null;
+		public PathReturnEval(final Map<Node, Node> myNodes,
+				final Map<Node, Node> otherNodes) {
+			this.myNodes = myNodes;
+			this.otherNodes = otherNodes;
+		}
 
-        public PathReturnEval( final Map<Node,Node> myNodes,
-            final Map<Node,Node> otherNodes )
-        {
-            this.myNodes = myNodes;
-            this.otherNodes = otherNodes;
-        }
+		public boolean accept(Path currentPos) {
+			Node currentNode = currentPos.endNode();
+			Node prevNode = currentPos.lastRelationship().getOtherNode(
+					currentNode);
+			if (!otherNodes.containsKey(currentNode)) {
+				myNodes.put(currentNode, prevNode);
+			} else {
+				match = new LinkedList<Node>();
+				match.add(currentNode);
+				while (prevNode != null) {
+					match.add(prevNode);
+					prevNode = myNodes.get(prevNode);
+				}
+				Node otherNode = otherNodes.get(currentNode);
+				while (otherNode != null) {
+					match.addFirst(otherNode);
+					otherNode = otherNodes.get(otherNode);
+				}
+			}
+			return true;
+		}
 
-        public boolean isReturnableNode( TraversalPosition currentPos )
-        {
-            Node currentNode = currentPos.currentNode();
-            Node prevNode = currentPos.previousNode();
-            if ( !otherNodes.containsKey( currentNode ) )
-            {
-                myNodes.put( currentNode, prevNode );
-            }
-            else
-            {
-                match = new LinkedList<Node>();
-                match.add( currentNode );
-                while ( prevNode != null )
-                {
-                    match.add( prevNode );
-                    prevNode = myNodes.get( prevNode );
-                }
-                Node otherNode = otherNodes.get( currentNode );
-                while ( otherNode != null )
-                {
-                    match.addFirst( otherNode );
-                    otherNode = otherNodes.get( otherNode );
-                }
-            }
-            return true;
-        }
+		protected List<Node> getMatch() {
+			return match;
+		}
 
-        protected List<Node> getMatch()
-        {
-            return match;
-        }
-    }
+	}
 }
